f2e3c09a6895e58b254d2d6d0291304f5e3cc442
Mahesh Pattar <mahesh.pattar@ltts.com>
2020-07-31 16:22:03 +0530

    Add project files.

---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageFromClient", user, message);
+        }
+    }
+}
